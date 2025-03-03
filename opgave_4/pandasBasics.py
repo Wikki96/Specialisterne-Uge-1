7ba84ebb32b385c7b6184fe83740cc3b2b3cb497
Viktor Hjort <vfhjort@gmail.com>
2025-03-03 14:59:13 +0100

    Opgave 4 done

---
+++ opgave_4/pandasBasics.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("opgave_4/DKHousingPricesSample100k.csv")
+#print(data.head(10))
+dataByRegion = data.groupby("region")
+averagePurchasePriceByRegion = dataByRegion.mean("purchase_price")["purchase_price"]
+print(averagePurchasePriceByRegion)
+dataByHouseTypeAndRegion = data.groupby(["house_type", "region"])
+plotData = dataByHouseTypeAndRegion.mean("purchase_price")["purchase_price"]
+plotData.unstack().plot(kind= "bar")
+plotData.unstack().plot(kind= "bar", subplots=True, layout=(2,2))
+plt.show()
